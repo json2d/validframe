d3ad7201b0fa462f537295ea5ef3897a00585446
Jason Yung <json.yung@gmail.com>
2020-03-18 16:26:36 -0400

    bump semver

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'validframe', # How you named your package folder (MyLib)
   packages = ['validframe'], # Chose the same as "name"
-  version = '0.1.1', # Start with a small number and increase it with every change you make
+  version = '0.2.0', # Start with a small number and increase it with every change you make
   license= 'MIT', # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'validium validators for pandas dataframes', # Give a short description about your library
   author = 'Jason Yung',
   author_email = 'json.yung@gmail.com',
   url = 'https://github.com/json2d/validframe', # Provide either the link to your github or to your website
-  download_url = 'https://github.com/json2d/validframe/archive/v0.1.1.tar.gz',
+  download_url = 'https://github.com/json2d/validframe/archive/v0.2.0.tar.gz',
   keywords = ['validation', 'dataframe', 'pandas', 'validium', 'rambda'], # Keywords that define your package best
   install_requires= ['validium', 'ramda'],
   classifiers=[
